3d7c7dd5af8a32653c45ec85a66bd78484263e00
IrshadCodes7 <irshadmd92236@gmail.com>
2026-04-21 17:29:27 +0530

    Add rendering logic of each module i.e. home,menu and contact

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import { homePage } from './home.js';
 import { menuPage } from './menu.js';
 import {contactPage} from './contact.js';
+
 homePage();
-// menuPage();
-// contactPage();
+const nav = document.querySelector("nav");
+
+nav.addEventListener('click',(e)=>{
+    if(e.target.id == 'home'){
+        homePage();
+    }
+    if(e.target.id == 'menu'){
+        menuPage();
+    }
+    if(e.target.id == 'contact'){
+        contactPage();
+    }
+})
